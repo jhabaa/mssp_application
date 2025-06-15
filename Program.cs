48b300ad3dbe960ba76e144e211a6f8c101b5cb6
Hean <jhubert@heanlab.com>
2025-06-15 16:43:35 +0200

    [FEATURE]: Loading secrets keys

--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    // Load secrets from environment variables
+    builder.Configuration.AddUserSecrets<Program>(optional: true);
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
