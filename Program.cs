d20e28c9593a2eb457ba3b037c3123b6812d2a32
Jean Hubert ABA'A <jhubert@heanlab.com>
2025-06-16 15:21:02 +0200

    [FIX]: Load environments variables in production for github action and secret otherwise

--- Program.cs
+++ Program.cs
@@ -3,18 +3,28 @@ using mssp_application.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Load envs datas ( for Github actions)
+builder.Configuration.AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
 // Add Mongo DB conneection & datas as a singleton service. 
 // Then you can inject it into your components or pages.
 builder.Services.AddSingleton<MongoConnection>();
+
 //Launch app on port 5010
 builder.WebHost.UseUrls("http://0.0.0.0:5010");
-var app = builder.Build();
 
-// Load secrets from environment variables
-builder.Configuration.AddUserSecrets<Program>(optional: true);
+if(builder.Environment.IsDevelopment())
+{
+    // In development, load secrets from user secrets here
+    // Load secrets from environment variables
+    builder.Configuration.AddUserSecrets<Program>(optional: true);
+}
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
